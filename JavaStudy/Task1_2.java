5965f865e89a7c121da10c73027008ba00cea173
tanakadatearisa <tanakadatetanakadate@gmail.com>
2021-07-23 23:39:59 +0900

    課題提出テスト
    Task1_2.java

---
+++ JavaStudy/Task1_2.java
@@ -0,0 +1,35 @@
+
+public class Task1_2 { 
+    public static void main(String[] args) {
+        //①「JAPAN」「AMERICA」「KOREA」を要素の値(初期値)とする配列 countryを作成しなさい
+        String[] country = {"JAPAN", "AMERICA","KOREA"};
+        //② ①で作成した配列の要素数を出力してください。
+        System.out.println(country.length);
+        //③下記の値を保持した、要素数3のStringクラスの配列strArrayを作成しなさい。
+        //1番目(先頭)の要素に「りんご」を代入
+        //2番目の要素に「もも」を代入
+        //3番目の要素に「ぶどう」を代入
+        String[] strArray = {"りんご", "もも", "ぶどう"};
+        //④ ③で作成した配列の２番目の要素の出力しなさい
+        System.out.println(strArray[1]);
+
+        //⑤下記のコメント
+        int[] intArray = { 10, 20, 30, 40, 50 };
+        //⑥下記のコメント
+        System.out.println(intArray[1] + intArray[4]);
+        
+
+
+
+        
+        
+        
+
+         
+
+        
+    }
+    
+}
+
+
