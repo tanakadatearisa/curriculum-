9ffee03038a2ef74b1076b2b52ce7bedf14f53d3
tanakadatearisa <tanakadatetanakadate@gmail.com>
2021-07-30 01:49:09 +0900

    課題提出テスト
    Task1_3.java

---
+++ JavaStudy/Task1_3.java
@@ -0,0 +1,33 @@
+public class Task1_3 {
+    public static void main(String[] args){
+        //①5と10を加算した値を代入する、変数 plus を作成しなさい。
+        int plus = 0;
+        plus = 5+10;
+        
+        //②20 から 7 を減算した値を代入す、変数 minus を作成しなさい。
+        int minus = 0;
+        minus =20-7;
+        
+        //③下記の処理について、何をしているのかコメントを記入してください。
+        //10と2を掛け合わせてmltiplyに代入
+        int multiply = 10 * 2;
+        
+        //④20 を 6 で割った余りを代入する、変数 remi を作成しなさい。
+        int remi = 20 % 6;
+        
+        //③下記の処理について、何をしているのかコメントを記入してください。
+        //
+        String town = "有楽町";
+        String Line = "線";
+        //⑥ ⑤で作成した変数を連結させた値を代入する変数 train を作成しなさい。
+        String train = town + Line;
+        //⑦ 変数plus, mius, multiply, remi, train をそれぞれ出力しなさい
+        System.out.println(plus);
+        System.out.println(minus);
+        System.out.println(multiply);
+        System.out.println(remi);
+        System.out.println(train);
+        
+    }
+    
+}
